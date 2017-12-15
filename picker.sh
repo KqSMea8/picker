f78790e30a628b91bd6e7037cbe50e770e5ca8c8
Stuart Bennett <stubennett@uk.ibm.com>
2017-12-15 19:01:36 +0000

    Why is hedge fund on the list? List ETFs: epic,sector,charge order by charge asc Report on myepics: epic,sector,gain order by gain desc

--- picker.sh
+++ picker.sh
@@ -18,51 +18,24 @@ then
     exit 1
 fi
 
-mkdir -p ./tables
-rm -f ./tables/*
-echo "$(date) - Investment Trusts  >./tables/sector_epics.csv"
-echo "sector,epic" >./tables/sector_epics.csv
-lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/investment-trusts" |sed -n '/<select id="sectorid" name="sectorid"*/,/<\/select>/p' |grep 'option value' |grep -v '<option value="">Please select</option>' |grep -v VCT |sort |while read rec
+echo "$(date) - Investment Trusts"
+echo "epic,sector_desc" >>./invtrust_sectors.csv
+lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/investment-trusts" |sed -n '/<select id="sectorid" name="sectorid"*/,/<\/select>/p' |grep 'option value' |grep -v '<option value="">Please select</option>' |grep -v VCT |while read rec
 do
     sector_id=$(echo $rec |cut -f2 -d'"')
-    sector=$(echo $rec |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/\&amp;/\&/g' |sed 's/Sector Specialist: //g')
+    sector_desc=$(echo $rec |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/\&amp;/\&/g' |sed 's/Sector Specialist: //g')
     lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/investment-trusts/search-for-investment-trusts?companyid=&tab=prices&sectorid=${sector_id}&tab=prices&it_search_input=" |grep shares-search-results |cut -f6 -d"/" |cut -f1 -d'"' |sort |uniq |while read epic
     do
-        echo "$sector,$epic" >>./tables/sector_epics.csv
+        echo "$epic,$sector_desc" >>./invtrust_sectors.csv
     done
 done
 
-echo "$(date) - ETFs  >./sector_epics.csv"
-rm -f ./sort.tmp ./etfs.tmp
-lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/exchange-traded-funds-etfs" |sed -n '/<option value="">Please select sector<\/option>/,/<\/select>/p' |grep 'option value' |grep -v '<option value="">Please select sector</option>' |sort |egrep 'Commodity|Equity' |while read rec
+echo "$(date) - Downloading Investment Trust Details"
+rm -fR ./invtrusts
+mkdir ./invtrusts
+./q -H -d',' "select distinct epic from ./invtrust_sectors.csv" |while read epic
 do
-    sector_id=$(echo $rec |cut -f2 -d'"')
-    sector=$(echo $rec |cut -f2 -d'>' |cut -f1 -d'<' |cut -f2 -d'-' |sed 's/\&amp;/\&/g' |sed 's/Banks \& //g' |sed 's/& South //g' |sed "s/^ //")
-    it_sector=$(grep "$sector" ./tables/unfashionable_sectors.csv |head -1 |cut -f1 -d',')
-    if [ -n "$it_sector" ]
-    then
-        seq 0 50 500 |while read offset
-        do
-          lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/exchange-traded-funds-etfs/list-of-etfs?offset=${offset}&etf_search_input=&companyid=&sectorid=${sector_id}&non_soph=1"  |grep shares-search-results |cut -f6 -d"/" |cut -f1 -d'"' |while read epic
-            do
-                echo "$sector,$epic" >>./sort.tmp
-            done
-        done
-        echo "sector: $it_sector sort.tmp: $(wc -l ./sort.tmp)"
-        sort ./sort.tmp |uniq >>./tables/sector_epics.csv
-        cat ./sort.tmp |cut -f2 -d',' >>./etfs.tmp
-    fi
-    rm -f ./sort.tmp
-done
-
-
-
-echo "$(date) - Downloading details"
-rm -fR ./tmp
-mkdir ./tmp
-./q -H -d',' "select epic from ./tables/sector_epics.csv" |while read epic
-do
-    lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/shares-search-results/$epic" |sed -n '/<table class="factsheet-table/,/<\/table>/p' |tidy >./tmp/${epic}.html 2>/dev/null &
+    lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/shares-search-results/$epic" |sed -n '/<table class="factsheet-table/,/<\/table>/p' |tidy >./invtrusts/${epic}.html 2>/dev/null &
     while [ $(pgrep -f lynx |wc -l) -gt 20 ]
     do
         sleep 1
@@ -73,39 +46,51 @@ do
    sleep 1
 done
 
-echo "$(date) - Generating ./tables/epic_details.csv"
-echo "epic,pd,pd12,charge" >./tables/epic_details.csv
-for file in ./tmp/*
+echo "$(date) - Generating ./invtrust_details.csv"
+echo "epic,pd,pd12,charge" >./invtrust_details.csv
+./q -H -d',' "select distinct epic from ./invtrust_sectors.csv" |while read rec
 do
-    epic=$(basename $file .html)
+    epic=$(echo "$rec" |cut -f1 -d',')
+    file=./invtrusts/${epic}.html
     charge=$(grep -A1 -i 'ongoing charge' $file |tail -1 |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/n\/a//g' |sed 's/%//g')
     pd=$(egrep -A1 -i '^premium'  $file |tail -1 |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/n\/a//g' |sed 's/%//g')
     pd12=$(egrep -A1 -i '^12m average premium'  $file |tail -1 |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/n\/a//g' |sed 's/%//g')
-    # if [ -n "$charge" ]
-    # then
-        echo "$epic,$pd,$pd12,$charge" >>./tables/epic_details.csv
-    # else
-    #     echo "No charge for http://www.hl.co.uk/shares/shares-search-results/$epic in $file"
-    # fi
+    echo "$epic,$pd,$pd12,$charge" >>./invtrust_details.csv
 done
-rm -fR ./tmp/
 
-./q -O -H -d',' "select sector from ./tables/epic_details.csv ed join ./tables/sector_epics.csv se on ed.epic = se.epic
-  where ed.epic in (select epic from ./tables/sector_epics.csv group by epic having count(*) = 1)
+./q -O -H -d',' "select es.sector_desc from ./invtrust_sectors.csv es join ./invtrust_details.csv id on es.epic = id.epic
+  where es.epic in (select epic from ./invtrust_sectors.csv group by epic having count(*) = 1)
   and pd12 > -90 and pd12 < 100
-  group by sector having count(pd12) > 2 order by avg(pd12)" |head -7 >./tables/unfashionable_sectors.csv
-
-
-exit 0
+  group by sector_desc having count(pd12) > 2 order by avg(pd12)" |head -7 >./unfashionable_sectors.csv
 
 
 
-echo "$(date) - Downloading details"
-rm -fR ./tmp
-mkdir ./tmp
-./q -H -d',' "select epic from ./sector_epics.csv" |while read epic
+echo "$(date) - ETFs"
+echo "epic,sector_desc" >./etf_sectors.csv
+rm -f ./etf_sectors.tmp
+lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/exchange-traded-funds-etfs" \
+  |sed -n '/<option value="">Please select sector<\/option>/,/<\/select>/p' \
+  |grep 'option value' |grep -v '<option value="">Please select sector</option>' |while read rec
 do
-    lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/shares-search-results/$epic" |sed -n '/<table class="factsheet-table/,/<\/table>/p' |tidy >./tmp/${epic}.html 2>/dev/null &
+    sector_id=$(echo $rec |cut -f2 -d'"')
+    sector_desc=$(echo $rec |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/\&amp;/\&/g' |sed "s/^ //" |sed 's/,//g')
+    seq 0 50 500 |while read offset
+    do
+        lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/exchange-traded-funds-etfs/list-of-etfs?offset=${offset}&etf_search_input=&companyid=&sectorid=${sector_id}&non_soph=1"  |grep shares-search-results |cut -f6 -d"/" |cut -f1 -d'"' |while read epic
+        do
+            echo "$epic,$sector_desc" >>./etf_sectors.tmp
+        done
+    done
+done
+cat ./etf_sectors.tmp |sort |uniq >>./etf_sectors.csv
+rm -f ./etf_sectors.tmp
+
+echo "$(date) - Downloading ETF Details"
+rm -fR ./etfs
+mkdir ./etfs
+./q -H -d',' "select substr('0000000'||epic, -7, 7) from ./etf_sectors.csv group by epic having count(*) = 1" |while read epic
+do
+    lynx -source -useragent="$useragent" "http://www.hl.co.uk/shares/shares-search-results/$epic" |sed -n '/<table class="factsheet-table/,/<\/table>/p' |tidy >./etfs/${epic}.html 2>/dev/null &
     while [ $(pgrep -f lynx |wc -l) -gt 20 ]
     do
         sleep 1
@@ -116,7 +101,10 @@ do
    sleep 1
 done
 
-echo "epic,pd,pd12,charge" >./epic_details.csv
+echo "$(date) - Finished"
+exit 0
+
+echo "epic,charge" >./etf_details.csv
 for file in ./tmp/*
 do
     epic=$(basename $file .html)
@@ -129,7 +117,7 @@ do
     pd12=$(egrep -A1 -i '^12m average premium'  $file |tail -1 |cut -f2 -d'>' |cut -f1 -d'<' |sed 's/n\/a//g' |sed 's/%//g')
     if [ -n "$charge" ]
     then
-        echo "$epic,$pd,$pd12,$charge" >>./epic_details.csv
+        echo "$epic,$pd,$pd12,$charge" >>./invtrust_details.csv
     else
         echo "No charge for http://www.hl.co.uk/shares/shares-search-results/$epic in $file"
     fi
@@ -137,18 +125,19 @@ done
 
 rm -fR ./tmp/ ./sectors.csv
 
-./q -O -H -d',' "select sector from ./epic_details.csv ed join ./sector_epics.csv se on ed.epic = se.epic
+./q -O -H -d',' "select sector from ./invtrust_details.csv ed join ./sector_epics.csv se on ed.epic = se.epic
   where ed.epic in (select epic from ./sector_epics.csv group by epic having count(*) = 1)
   and pd12 > -90 and pd12 < 100
   group by sector having count(pd12) > 2 order by avg(pd12)" |head -7 >./unfashionable_sectors.csv
 
-./q -H -d',' "select sector from ./unfashionable_sectors.csv" |while read sector
+./q -H -d',' "select sector_desc from ./unfashionable_sectors.csv" |while read sector_desc
 do
     echo
-    echo "$sector"
+    echo "$sector_desc"
     echo "================================"
     echo
-    ./q -O -H -d',' "select 'http://www.hl.co.uk/shares/shares-search-results/'||substr('0000000'||ed.epic, -7, 7) url,pd discount,charge from ./epic_details.csv ed join ./sector_epics.csv se on ed.epic = se.epic where se.sector = '$sector' and charge < 2 order by charge,pd" |column -s ',' -t
+    ./q -O -H -d',' "select 'http://www.hl.co.uk/shares/shares-search-results/'||substr('0000000'||id.epic, -7, 7) url,pd discount,charge from ./invtrust_details.csv id
+           join ./invtrust_sectors.csv isec on id.epic = isec.epic where isec.sector_desc = '$sector_desc' and charge < 2 order by charge,pd" |column -s ',' -t
 done
 
 echo "epic,gain" >./myepics.csv
