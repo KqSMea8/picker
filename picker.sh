6c41480804bfcd84ee4b79f29ef9815ba50ce74e
Stuart Bennett <stubennett@uk.ibm.com>
2017-12-16 14:10:15 +0000

    Working

--- picker.sh
+++ picker.sh
@@ -61,15 +61,19 @@ then
         echo "$epic,$pd,$pd12,$charge" >>./invtrust_details.csv
     done
 
-    ./q -O -H -d',' "select es.sector_desc from ./invtrust_sectors.csv es
+    ./q -O -H -d',' "select es.sector_desc, avg(pd12) avg from ./invtrust_sectors.csv es
       join ./invtrust_details.csv id on es.epic = id.epic
       where pd12 > -90 and pd12 < 100
-      group by es.sector_desc having count(*) > 2 order by avg(pd12)" |head -7 >./unfashionable_sectors.csv
+      group by es.sector_desc having count(*) > 2" >./unfashionable_sectors.csv
 
 fi
 
-./q -H -d',' "select sector_desc from ./unfashionable_sectors.csv" 2>/dev/null |while read sector_desc
-do
+./q -H -d',' "select isec.sector_desc from ./invtrust_sectors.csv isec
+  join ./unfashionable_sectors.csv us on (isec.sector_desc = us.sector_desc)
+  join ./invtrust_details.csv id on (isec.epic = id.epic)
+  where id.charge < 2
+  group by isec.sector_desc order by us.avg" |head -7 |while read sector_desc
+  do
     echo
     echo "$sector_desc"
     echo "================================"
@@ -77,6 +81,8 @@ do
     ./q -O -H -d',' "select distinct 'http://www.hl.co.uk/shares/shares-search-results/'||substr('0000000'||id.epic, -7, 7) url,pd discount,charge from ./invtrust_details.csv id
            join ./invtrust_sectors.csv isec on id.epic = isec.epic where isec.sector_desc = '$sector_desc' and charge < 2 order by charge,pd" |column -s ',' -t
 done
+
+rm -f ./invtrusts/
 
 echo
 echo "Sell if gain > 24"
