3d68e4b5dee8e01a9ad960731cd15268e3fd6be6
bennetts <bennetts@localhost.localdomain>
2019-03-19 15:46:23 +0000

    fix

--- stocks.py
+++ stocks.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 # Using https://pypi.org/project/yahoofinancials/
 from yahoofinancials import YahooFinancials
+from finsymbols import symbols
 
-ticker = 'AAPL'
-yahoo_financials = YahooFinancials(ticker)
+pyEX.symbolsList()
+
+for stock in symbols.get_nasdaq_symbols():
+
+
+Stock = YahooFinancials(stock)
 
 yahoo_financials.get_financial_stmts('quarterly', 'balance')
